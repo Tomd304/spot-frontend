bb7571e68c449ea91608f096e07990b535bce00b
Tomd304 <tomd304@live.co.uk>
2022-05-05 16:16:27 +0100

    added await for token

--- src/App.js
+++ src/App.js
@@ -12,19 +12,23 @@ function App() {
     console.log("getting token");
     console.log("id: " + process.env.REACT_APP_CLIENT_ID);
     console.log("secret: " + process.env.REACT_APP_CLIENT_SECRET);
-    axios("https://accounts.spotify.com/api/token", {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        Authorization:
-          "Basic " + btoa(spotify.ClientId + ":" + spotify.ClientSecret),
-      },
-      data: "grant_type=client_credentials",
-      method: "POST",
-    }).then((tokenResponse) => {
+    const callToken = async () => {
+      const tokenResponse = await axios(
+        "https://accounts.spotify.com/api/token",
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Authorization:
+              "Basic " + btoa(spotify.ClientId + ":" + spotify.ClientSecret),
+          },
+          data: "grant_type=client_credentials",
+          method: "POST",
+        }
+      );
       setToken("Bearer " + tokenResponse.data.access_token);
       console.log("token set : " + token);
       setGettingToken(false);
-    });
+    };
   }, [spotify.ClientId, spotify.ClientSecret]);
 
   return <>{!gettingToken ? <Dashboard token={token} /> : null}</>;
