029f7bebb0b70e1dfef3fb2bbe534941143c4dca
Tomd304 <tomd304@live.co.uk>
2022-05-05 15:56:36 +0100

    ENV file variable reference updated

--- src/Credentials.js
+++ src/Credentials.js
@@ -1,7 +1,7 @@
 const Credentials = () => {
   return {
-    REACT_APP_CLIENT_ID: process.env.ClientId,
-    REACT_APP_CLIENT_SECRET: process.env.ClientId,
+    REACT_APP_CLIENT_ID: process.env.REACT_APP_CLIENT_ID,
+    REACT_APP_CLIENT_SECRET: process.env.REACT_APP_CLIENT_SECRET,
   };
 };
 
