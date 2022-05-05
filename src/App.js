c61740abecab2f49a50d5191638963353340bc9e
Tomd304 <tomd304@live.co.uk>
2022-05-05 16:28:09 +0100

    env references updated

--- src/App.js
+++ src/App.js
@@ -8,27 +8,39 @@ function App() {
   const [token, setToken] = useState("");
   const [gettingToken, setGettingToken] = useState(true);
   const spotify = Credentials();
-  useEffect(async () => {
+  useEffect(() => {
     console.log("getting token");
     console.log("id: " + process.env.REACT_APP_CLIENT_ID);
     console.log("secret: " + process.env.REACT_APP_CLIENT_SECRET);
+
     const callToken = async () => {
-      const tokenResponse = await axios(
-        "https://accounts.spotify.com/api/token",
-        {
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            Authorization:
-              "Basic " + btoa(spotify.ClientId + ":" + spotify.ClientSecret),
-          },
-          data: "grant_type=client_credentials",
-          method: "POST",
-        }
-      );
-      setToken("Bearer " + tokenResponse.data.access_token);
-      console.log("token set : " + token);
-      setGettingToken(false);
+      try {
+        const tokenResponse = await axios(
+          "https://accounts.spotify.com/api/token",
+          {
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+              Authorization:
+                "Basic " +
+                btoa(
+                  process.env.REACT_APP_CLIENT_ID +
+                    ":" +
+                    process.env.REACT_APP_CLIENT_SECRET
+                ),
+            },
+            data: "grant_type=client_credentials",
+            method: "POST",
+          }
+        );
+        console.log(tokenResponse);
+        setToken("Bearer " + tokenResponse.data.access_token);
+        console.log("token set : " + token);
+        setGettingToken(false);
+      } catch (e) {
+        console.log(e);
+      }
     };
+    callToken();
   }, [spotify.ClientId, spotify.ClientSecret]);
 
   return <>{!gettingToken ? <Dashboard token={token} /> : null}</>;
