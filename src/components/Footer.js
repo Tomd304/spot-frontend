092c1eeba7d1479745703857484b9df7868eda2e
Tomd304 <tomd304@live.co.uk>
2022-05-17 22:06:23 +0100

    Git link added to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,18 @@
 import "./Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { brands } from "@fortawesome/fontawesome-svg-core/import.macro"; // <-- import styles to be used
 
 const Footer = (props) => {
-  return <div className="footer"></div>;
+  return (
+    <div className="footer">
+      <a href="https://github.com/Tomd304">
+        <FontAwesomeIcon
+          style={{ cursor: "pointer", fontSize: "1.75em" }}
+          icon={brands("github")}
+        />
+      </a>
+    </div>
+  );
 };
 
 export default Footer;
